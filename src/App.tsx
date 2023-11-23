bc888a902b0e4ff95b97be1f3a396ed6ec3786fc
liuxin <liuxin85@gmail.com>
2023-11-23 20:44:28 +0800

    react-jike

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
