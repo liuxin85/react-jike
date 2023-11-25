456570d0c8b4daafdcf2360ce025027e2e89433d
liuxin <liuxin85@gmail.com>
2023-11-25 15:53:43 +0800

    二级路由配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/index.tsx'
-import { Provider } from 'react-redux'
-import store from './store/index.ts'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/index.tsx";
+import { Provider } from "react-redux";
+import store from "./store/index.ts";
+import "normalize.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-)
+);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,40 @@
-import AuthRoute from '@/components/AuthRoute'
-import Layout from '@/pages/Layout'
-import Login from '@/pages/Login'
-import { createBrowserRouter } from 'react-router-dom'
+import AuthRoute from "@/components/AuthRoute";
+import Article from "@/pages/Article";
+import Home from "@/pages/Home";
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
+import Publish from "@/pages/Publish";
+import { createBrowserRouter } from "react-router-dom";
 
 // 配置路由实例
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: (
       <AuthRoute>
         <Layout />
       </AuthRoute>
     ),
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "article",
+        element: <Article />,
+      },
+      {
+        path: "publish",
+        element: <Publish />,
+      },
+    ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
-])
+]);
 
-export default router
+export default router;
