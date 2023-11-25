a19723940e81ed348b1f53db1e74d649d8fe8e8b
liuxin <liuxin85@gmail.com>
2023-11-25 18:15:32 +0800

    处理token失效

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,15 +1,16 @@
 // axios封装处理
-import axios from 'axios'
-import { getToken } from '.'
+import axios from "axios";
+import { getToken, removeToken } from ".";
+import router from "@/router";
 
 // 1. 根域名配置
 // 2. 超时时间
 // 3. 请求拦截器/响应拦截器
 
 const request = axios.create({
-  baseURL: 'http://geek.itheima.net/v1_0',
+  baseURL: "http://geek.itheima.net/v1_0",
   timeout: 5000,
-})
+});
 
 // 添加请求拦截器
 request.interceptors.request.use(
@@ -17,29 +18,36 @@ request.interceptors.request.use(
     // 操作这个config 注入token数据
     // 1. 注入token
     // 2. 按照后端的格式要求做token 拼接
-    const token = getToken()
+    const token = getToken();
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // 添加响应拦截器
 request.interceptors.response.use(
   (response) => {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
-    return response.data
+    return response.data;
   },
   (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
-    return Promise.reject(error)
+    // 监控401 token 失效
+    console.dir(error);
+    if (error.response.status === 401) {
+      removeToken();
+      router.navigate("/login");
+      window.location.reload();
+    }
+    return Promise.reject(error);
   }
-)
+);
 
-export { request }
+export { request };
