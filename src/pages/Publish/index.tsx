820993e187fcfe085db6f1d91b6921278871a7e7
liuxin <liuxin85@gmail.com>
2023-11-26 10:21:56 +0800

    发布-频道列表的基础渲染

--- src/pages/Publish/index.tsx
+++ src/pages/Publish/index.tsx
@@ -5,10 +5,23 @@ import "./index.scss";
 
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { getChannelAPI } from "@/apis/article";
 
 const { Option } = Select;
 
 const Publish = () => {
+  const [channelList, setChannelList] = useState([]);
+
+  useEffect(() => {
+    // 1. 封装函数 在函数内部调用接口
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data.channels);
+    };
+    // 2. 调用函数
+    getChannelList();
+  }, []);
   return (
     <div className="publish">
       <Card
@@ -27,7 +40,12 @@ const Publish = () => {
             name="channel_id"
             rules={[{ required: true, message: "请选择文章频道" }]}>
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {/* value 属性用户选中后会自动收集起来作为接口提交的字段 */}
+              {channelList.map((item) => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item
