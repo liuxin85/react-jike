1dc64bccb574f74b1a5b1311526783aa420137d7
liuxin <liuxin85@gmail.com>
2023-11-26 13:01:20 +0800

    封面的基础上传

--- src/pages/Publish/index.tsx
+++ src/pages/Publish/index.tsx
@@ -6,12 +6,13 @@ import "./index.scss";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useEffect, useState } from "react";
-import { getChannelAPI } from "@/apis/article";
+import { createArticleAPI, getChannelAPI } from "@/apis/article";
 
 const { Option } = Select;
 
 const Publish = () => {
   const [channelList, setChannelList] = useState([]);
+   const [imageList, setImageList] = useState([]);
 
   useEffect(() => {
     // 1. 封装函数 在函数内部调用接口
@@ -22,13 +23,39 @@ const Publish = () => {
     // 2. 调用函数
     getChannelList();
   }, []);
+
+  // 提交表单
+  const onFinish = (formValue) => {
+    console.log(formValue);
+    const { title, content, channel_id } = formValue;
+    // 1. 按照接口文档的格式处理收集到的表单数据
+    const reqData = {
+      title,
+      content,
+      cover: {
+        type: 0,
+        images: [],
+      },
+      channel_id,
+    };
+
+    // 2. 调用接口提交
+    createArticleAPI(reqData);
+  };
+    const onUploadChange = (info) => {
+      setImageList(info.fileList);
+    };
   return (
     <div className="publish">
       <Card
         title={
           <Breadcrumb items={[{ title: <Link to={"/"}>首页</Link> }, { title: "发布文章" }]} />
         }>
-        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }}>
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+          onFinish={onFinish}>
           <Form.Item
             label="标题"
             name="title"
@@ -47,6 +74,25 @@ const Publish = () => {
                 </Option>
               ))}
             </Select>
+          </Form.Item>
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Upload
+              name="image"
+              listType="picture-card"
+              showUploadList
+              action={"http://geek.itheima.net/v1_0/upload"}
+              onChange={onUploadChange}>
+              <div style={{ marginTop: 8 }}>
+                <PlusOutlined />
+              </div>
+            </Upload>
           </Form.Item>
           <Form.Item
             label="内容"
